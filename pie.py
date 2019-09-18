da3ca748654d85c85940aec25fddf399d5ca9e50
zkxshg <35519607+zkxshg@users.noreply.github.com>
2019-09-18 16:38:40 +0800

    Create pie.py

---
+++ pie.py
@@ -0,0 +1,15 @@
+from pyecharts.faker import Faker
+from pyecharts import options as opts
+from pyecharts.charts import Page, Pie
+
+def pie_base() -> Pie:
+    c = (
+        Pie()
+        .add("", [list(z) for z in zip(Faker.choose(), Faker.values())])
+        .set_global_opts(title_opts=opts.TitleOpts(title="Pie-基本示例"))
+        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    )
+    return c
+
+pie = pie_base()
+pie.render('pie.html')
